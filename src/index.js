3dfb314c9506fe91548ef901072b5a422d5bbe39
Marco Braggion <Marco@MacBook-Pro-di-Marco-2.local>
2023-11-03 17:01:43 +0100

    Switched BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <HashRouter>
+      {/* <BrowserRouter> */}
+
+      <App />
+      {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>
 );

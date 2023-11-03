6ee0c8fa9ec2e4ac0d2412429f107f841136a975
Marco Braggion <Marco@MacBook-Pro-di-Marco-2.local>
2023-11-03 18:14:32 +0100

    Changing HashRouter basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   <>
   <Header />
   <Routes>
-    <Route exact path="/" element={<Home/>}/>
+    <Route path="/" element={<Home/>}/>
     <Route path="/about-me" element={<AboutMe />}/>
     <Route path="/project/:name" element={<Project />}/>
   </Routes>

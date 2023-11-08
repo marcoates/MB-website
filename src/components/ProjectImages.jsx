6d22d6a92f6daf2d08b7a7e8068fb9dd92363571
Marco Braggion <Marco@MacBook-Pro-di-Marco-2.local>
2023-11-08 19:45:51 +0100

    Fixed issue in ToDoList Project page

--- src/components/ProjectImages.jsx
+++ src/components/ProjectImages.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { projects } from "../components/projects";
 import "./ProjectImages.css";
 import YoutubeEmbed from "./YoutubeEmbed";
+import { useMediaQuery } from "@mui/material";
 
 function ProjectImages(props) {
+  const isSmallScreen = useMediaQuery("(max-width:1000px)");
+
   return (
     <div className="projectImagesContainer">
       {projects.map((item, index) =>
@@ -25,12 +28,12 @@ function ProjectImages(props) {
               item.images.map((img, i) => (
               <div
               key={i} 
-              className={"projectImagesItemSafari projectImagesItemSafari"+item.id}
+              className={isSmallScreen ? "projectImagesItemSafari projectImagesItemSafari"+item.id : "projectImagesItem projectImagesItem"+item.id}
               id="imageMod">
                 <img
                   alt="proj-logo-img"
                   src={img}
-                  className={"projectImagesSafari projectImagesSafari"+item.id}
+                  className={isSmallScreen ? "projectImagesSafari projectImagesSafari"+item.id : "projectImages projectImages"+item.id}
                 ></img>
               </div>
             ))
